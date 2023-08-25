fed145839067a0923966b69e64022bb2ace29587
spawar1999 <120244512+spawar1999@users.noreply.github.com>
2023-08-26 00:00:50 +0530

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -6,7 +6,7 @@ email_recipient="spawar@securityhq.com"
 
 load_avg=$(cat /proc/loadavg | awk '{print $1}')
 
-if (( $(echo "$load_avg > $load_avg_threshold" | bc -l) )); then
+if (( $(echo "$load_avg -gt $load_avg_threshold" | bc -l) )); then
     echo "High load average detected on $HOSTNAME is $load_avg" | mail -s "Server Alert" "$email_recipient"
 fi
 
