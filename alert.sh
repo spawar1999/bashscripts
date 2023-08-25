bef05bacdbc03774bfacbb2e8c87ae940b02a688
spawar1999 <saurabhnpawar@outlook.com>
2023-08-26 00:00:01 +0530

    script

--- alert.sh
+++ alert.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 
 load_avg_threshold=4.0
+
 email_recipient="spawar@securityhq.com"
 
 load_avg=$(cat /proc/loadavg | awk '{print $1}')
 
 if (( $(echo "$load_avg > $load_avg_threshold" | bc -l) )); then
-    echo "High load average detected on $(hostname) - $load_avg" | mail -s "Server Alert" "$email_recipient"
+    echo "High load average detected on $HOSTNAME is $load_avg" | mail -s "Server Alert" "$email_recipient"
 fi
 
